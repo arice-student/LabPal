5739c6b2d07eafbbffe4393b1adfaa41a241e2eb
Anthony Rice <arice@madisoncollege.edu>
2021-03-03 12:51:47 -0600

    Removed all getter and setter methods from User class since Lombok is enabled.

--- src/main/java/edu/matc/entity/User.java
+++ src/main/java/edu/matc/entity/User.java
@@ -54,118 +54,6 @@ public class User {
     private Set<Role> roles = new HashSet<>();
 
     /**
-     * Instantiates a new User.
-     */
-    public User() {
-    }
-
-    /**
-     * Instantiates a new User.
-     *
-     * @param firstName   the first name
-     * @param lastName    the last name
-     * @param userName    the user name
-     * @param dateOfBirth the date of birth
-     */
-    public User(String firstName, String lastName, String userName, LocalDate dateOfBirth) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    /**
-     * Gets first name.
-     *
-     * @return the first name
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Sets first name.
-     *
-     * @param firstName the first name
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Gets last name.
-     *
-     * @return the last name
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Sets last name.
-     *
-     * @param lastName the last name
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Gets user name.
-     *
-     * @return the user name
-     */
-    public String getUserName() {
-        return userName;
-    }
-
-    /**
-     * Sets user name.
-     *
-     * @param userName the user name
-     */
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    /**
-     * Gets id.
-     *
-     * @return the id
-     */
-    public int getId() {
-        return id;
-    }
-
-
-    /**
-     * Sets id.
-     *
-     * @param id the id
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * Gets date of birth.
-     *
-     * @return the date of birth
-     */
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    /**
-     * Sets date of birth.
-     *
-     * @param dateOfBirth the date of birth
-     */
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    /**
      * Gets age.
      *
      * @return the age
@@ -173,32 +61,6 @@ public class User {
     public int getAge() {
 
         return (int)ChronoUnit.YEARS.between(dateOfBirth, LocalDate.now());
-    }
-
-    /**
-     * Gets orders.
-     *
-     * @return the orders
-     */
-    public Set<Order> getOrders() {
-        return orders;
-    }
-
-    /**
-     * Sets orders.
-     *
-     * @param orders the orders
-     */
-    public void setOrders(Set<Order> orders) {
-        this.orders = orders;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
     }
 
     /**
@@ -239,36 +101,5 @@ public class User {
     public void removeRole(Role role) {
         roles.remove(role);
         role.setUser(null);
-    }
-
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", userName='" + userName + '\'' +
-                ", id=" + id +
-                ", dateOfBirth=" + dateOfBirth +
-                ", age=" + getAge() +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id &&
-                Objects.equals(firstName, user.firstName) &&
-                Objects.equals(lastName, user.lastName) &&
-                Objects.equals(userName, user.userName) &&
-                Objects.equals(dateOfBirth, user.dateOfBirth);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(firstName, lastName, userName, id, dateOfBirth);
     }
 }
