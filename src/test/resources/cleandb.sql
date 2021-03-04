1857f13f099e5d1153967a1315c74843d233af6e
Anthony Rice <arice@madisoncollege.edu>
2021-03-04 07:18:34 -0600

    Most of the tests now pass for UserDaoTest.  For some reason, the user_id field was removed fromt he test_labpal role table.  Will focus on fixing the remaining issues.

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -2,5 +2,5 @@ delete from role;
 delete from user;
 delete from client;
 INSERT INTO user VALUES (1,'Joe','Coyne','jcoyne','supersecret1','1964-04-01'),(2,'Fred','Hensen','fhensen','supersecret2','1988-05-08'),(3,'Barney','Curry','bcurry','supersecret3','1947-11-11'),(4,'Karen','Mack','kmack','supersecret4','1986-07-08'),(5,'Dianne','Klein','dklein','supersecret5','1991-09-22'),(6,'Dawn','Tillman','dtillman','supersecret6','1979-08-30');
-INSERT INTO role VALUES (1,'admin','jcoyne');
+INSERT INTO role VALUES (1,'admin','jcoyne', 1);
 INSERT INTO client VALUES (1, 'Fruit'), (2, 'Vegetable'), (3, 'Dairy'), (4, 'Meat');
