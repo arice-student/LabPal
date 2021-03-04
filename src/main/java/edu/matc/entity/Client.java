eb1b61fd04ab0b7aeffee662d670e9bfe136f73e
Anthony Rice <arice@madisoncollege.edu>
2021-03-04 06:15:06 -0600

    Updated cleandb.sql to add client info.

---
+++ src/main/java/edu/matc/entity/Client.java
@@ -0,0 +1,24 @@
+package edu.matc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "Client")
+@Table(name = "client")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+}
--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,5 +1,6 @@
+delete from role;
 delete from user;
-delete from orders;
+delete from client;
 INSERT INTO user VALUES (1,'Joe','Coyne','jcoyne','supersecret1','1964-04-01'),(2,'Fred','Hensen','fhensen','supersecret2','1988-05-08'),(3,'Barney','Curry','bcurry','supersecret3','1947-11-11'),(4,'Karen','Mack','kmack','supersecret4','1986-07-08'),(5,'Dianne','Klein','dklein','supersecret5','1991-09-22'),(6,'Dawn','Tillman','dtillman','supersecret6','1979-08-30');
-INSERT INTO `orders` VALUES (1,'Plates',1),(2,'Books',2),(3,'Plates, forks, and knives ',2),(4,'Soccer ball',1),(5,'Coffee, Almond Milk, Granola',4),(6,'Bananas',1),(7,'Coffee, cups, and creamer',4);
-INSERT INTO `role` VALUES (1,'admin','jcoyne', 1);
+INSERT INTO role VALUES (1,'admin','jcoyne', 1);
+INSERT INTO client VALUES (1, 'Fruit'), (2, 'Vegetable'), (3, 'Dairy'), (4, 'Meat');
