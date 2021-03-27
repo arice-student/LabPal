6330a010449030d615ab00aee84da6a86f0a1c33
Anthony Rice <arice@madisoncollege.edu>
2021-03-27 09:53:06 -0500

    Changed the arice password throughout the application since I changed it earl it earlier this morning and the application now launches with all of the unit tests.

--- src/test/java/edu/matc/persistence/ClientDaoTest.java
+++ src/test/java/edu/matc/persistence/ClientDaoTest.java
@@ -1,105 +1,105 @@
-//package edu.matc.persistence;
-//
-//import edu.matc.entity.Client;
-//import edu.matc.test.util.Database;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// * Client Dao test
-// */
-//class ClientDaoTest {
-//
-//    GenericDao clientDao;
-//
-//    /**
-//     * Creating the dao.
-//     */
-//    @BeforeEach
-//    void setUp() {
-//        clientDao = new GenericDao(Client.class);
-//
-//        Database database = Database.getInstance();
-//        database.runSQL("cleandb.sql");
-//
-//    }
-//
-//    /**
-//     * Verifies gets all clients successfully.
-//     */
-//    @Test
-//    void getAll() {
-//        List<Client> clients = clientDao.getAll();
-//        assertEquals(4, clients.size());
-//    }
-//
-//
-//    /**
-//     * Verifies a client is returned correctly based on id search
-//     */
-//    @Test
-//    void getByIdSuccess() {
-//        Client client = new Client("Dairy");
-//        client.setId(3);
-//        Client retrievedClient = (Client)clientDao.getById(3);
-//        assertNotNull(retrievedClient);
-//        assertEquals(client, retrievedClient);
-//    }
-//
-//    /**
-//     * Verify successful insert of a client
-//     */
-//    @Test
-//    void insertSuccess() {
-//
-//        Client newClient = new Client("Carbs");
-//        int id = clientDao.insert(newClient);
-//        assertNotEquals(0,id);
-//        Client insertedClient = (Client)clientDao.getById(id);
-//        assertEquals(newClient, insertedClient);
-//    }
-//
-//    /**
-//     * Verify successful delete of client
-//     */
-//    @Test
-//    void deleteSuccess() {
-//        clientDao.delete(clientDao.getById(3));
-//        assertNull(clientDao.getById(3));
-//    }
-//
-//    /**
-//     * Verify successful update of client
-//     */
-//    @Test
-//    void updateSuccess() {
-//        String newClientName = "Carbs";
-//        Client clientToUpdate = (Client) clientDao.getById(3);
-//        clientToUpdate.setName(newClientName);
-//        clientDao.saveOrUpdate(clientToUpdate);
-//        Client retrievedClient = (Client) clientDao.getById(3);
-//        assertEquals(clientToUpdate, retrievedClient);
-//    }
-//
-//    /**
-//     * Verify successful get by property (equal match)
-//     */
-//    @Test
-//    void getByPropertyEqualSuccess() {
-//        List<Client> clients = clientDao.getByPropertyEqual("name", "fruit");
-//        assertEquals(1, clients.size());
-//        assertEquals(1, clients.get(0).getId());
-//    }
-//
-//    /**
-//     * Verify successful get by property (like match)
-//     */
-//    @Test
-//    void getByPropertyLikeSuccess() {
-//        List<Client> clients = clientDao.getByPropertyLike("name", "v");
-//        assertEquals(1, clients.size());
-//    }
-//}
+package edu.matc.persistence;
+
+import edu.matc.entity.Client;
+import edu.matc.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Client Dao test
+ */
+class ClientDaoTest {
+
+    GenericDao clientDao;
+
+    /**
+     * Creating the dao.
+     */
+    @BeforeEach
+    void setUp() {
+        clientDao = new GenericDao(Client.class);
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+
+    }
+
+    /**
+     * Verifies gets all clients successfully.
+     */
+    @Test
+    void getAll() {
+        List<Client> clients = clientDao.getAll();
+        assertEquals(4, clients.size());
+    }
+
+
+    /**
+     * Verifies a client is returned correctly based on id search
+     */
+    @Test
+    void getByIdSuccess() {
+        Client client = new Client("Dairy");
+        client.setId(3);
+        Client retrievedClient = (Client)clientDao.getById(3);
+        assertNotNull(retrievedClient);
+        assertEquals(client, retrievedClient);
+    }
+
+    /**
+     * Verify successful insert of a client
+     */
+    @Test
+    void insertSuccess() {
+
+        Client newClient = new Client("Carbs");
+        int id = clientDao.insert(newClient);
+        assertNotEquals(0,id);
+        Client insertedClient = (Client)clientDao.getById(id);
+        assertEquals(newClient, insertedClient);
+    }
+
+    /**
+     * Verify successful delete of client
+     */
+    @Test
+    void deleteSuccess() {
+        clientDao.delete(clientDao.getById(3));
+        assertNull(clientDao.getById(3));
+    }
+
+    /**
+     * Verify successful update of client
+     */
+    @Test
+    void updateSuccess() {
+        String newClientName = "Carbs";
+        Client clientToUpdate = (Client) clientDao.getById(3);
+        clientToUpdate.setName(newClientName);
+        clientDao.saveOrUpdate(clientToUpdate);
+        Client retrievedClient = (Client) clientDao.getById(3);
+        assertEquals(clientToUpdate, retrievedClient);
+    }
+
+    /**
+     * Verify successful get by property (equal match)
+     */
+    @Test
+    void getByPropertyEqualSuccess() {
+        List<Client> clients = clientDao.getByPropertyEqual("name", "fruit");
+        assertEquals(1, clients.size());
+        assertEquals(1, clients.get(0).getId());
+    }
+
+    /**
+     * Verify successful get by property (like match)
+     */
+    @Test
+    void getByPropertyLikeSuccess() {
+        List<Client> clients = clientDao.getByPropertyLike("name", "v");
+        assertEquals(1, clients.size());
+    }
+}
